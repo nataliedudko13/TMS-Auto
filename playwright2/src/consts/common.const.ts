99469675aa29bdd7eebaa8d2746e89869b7f812e
Natalie <natalie@Air-Natalie.bbrouter>
2024-08-06 19:51:23 +0300

    homework_playwright/allure

---
+++ playwright2/src/consts/common.const.ts
@@ -0,0 +1,12 @@
+export const BASE_URL = "https://www.typescriptlang.org/"
+export const DOWNLOAD_URL = "https://www.typescriptlang.org/download/"
+export const HANDBOOK_URL = "https://www.typescriptlang.org/docs/handbook/intro.html"
+export const TS_URL = "https://www.npmjs.com/package/typescript"
+export const API_URL = "https://demo.playwright.dev/api-mocking/"
+
+export const enum Pages {
+    StartPage,
+    NavigateBar,
+    DownloadPage,
+    HandbookPage
+  }
